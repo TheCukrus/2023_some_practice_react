083452b092d09c453e65b0b268cd83ef7ba40d0a
Jevgenij <sssgustavsss@gmail.com>
2023-02-08 13:30:18 +0200

    todo list item checkbox

--- Frontend/src/projects/todo_list_components/Todo_list_item.jsx
+++ Frontend/src/projects/todo_list_components/Todo_list_item.jsx
@@ -11,13 +11,32 @@ const Todo_list_item = (props) =>
         "completed": props.data[0].completed
     })
 
+    const update_todo_item = async (id, data) =>
+    {
+        try
+        {
+            const result1 = await axios(
+                {
+                    "method": "patch",
+                    "url": `http://127.0.0.1:80/api/v1/todo/${id}`,
+                    "data": data
+                }
+            )
+        }
+        catch (err)
+        {
+            console.log(err)
+        }
+    }
+
     const checkbox_on_change = async (e) =>
     {
         set_item_data({
             "title": props.data[0].title,
             "description": props.data[0].description,
             "completed": e.target.checked
-        })
+        });
+        await update_todo_item(props.data[0].title, item_data);
     }
 
     return (
