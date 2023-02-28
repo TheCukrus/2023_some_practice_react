251cf79c299d125e795e1423247aeb2fdafcbd88
Jevgenij <sssgustavsss@gmail.com>
2023-02-28 23:40:37 +0200

    continue work with Recipe_book_meal_by_id

--- Frontend/src/projects/recipe_book/Recipe_book_meal_by_id.jsx
+++ Frontend/src/projects/recipe_book/Recipe_book_meal_by_id.jsx
@@ -4,6 +4,17 @@ import s from "./Recipe_book.module.css";
 const Recipe_book_meal_by_id = (props) =>
 {
     console.log(props.meal)
+
+    let temp = new Array(20).fill({ measure: "", ingredient: "" });
+
+    for (let i = 1; i <= 20; i++)
+    {
+        const measure = props.meal[`strMeasure${i}`];
+        const ingredient = props.meal[`strIngredient${i}`];
+        if (!measure && !ingredient) break;
+        temp[i - 1] = { measure, ingredient };
+    }
+
     return (
         <div>
             <h1>there would be rendered meal by id</h1>
@@ -13,8 +24,26 @@ const Recipe_book_meal_by_id = (props) =>
 
                 {props.meal ? (
                     <div>
-                        <img src={props.meal.strMealThumb} />
+                        {/*renders img */}
+                        <img src={props.meal.strMealThumb} alt={props.meal.strMeal} />
+                        {/*renders name */}
                         <h1>{props.meal.strMeal}</h1>
+
+                        {/*renders ingredient's */}
+                        <h3>Ingredients</h3>
+                        <hr />
+
+                        {temp.map(({ measure, ingredient }, i) => (
+                            <p key={i}>
+                                {measure && `${measure} `}
+                                {ingredient}
+                            </p>
+                        ))}
+
+                        {/*renders instructions */}
+                        <h3>Instructions</h3>
+                        <hr />
+                        {props.meal.strInstructions}
                     </div>
                 ) : (
                     <p>Loading...</p>
